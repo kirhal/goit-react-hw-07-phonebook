9cc8d861b6058344c49e58367855184e83aeea2f
kirhal <kirillgaleckiy@gmail.com>
2023-05-22 10:33:51 +0200

    upd

--- src/redux/seleclors.js
+++ src/redux/seleclors.js
@@ -1,4 +1,4 @@
-import { createSelector } from '@reduxjs/toolkit';
+// import { createSelector } from '@reduxjs/toolkit';
 
 export const getContactsList = state => state.contacts.data;
 export const getIsLoading = state => state.contacts.isLoading;
@@ -8,7 +8,7 @@ export const selectFilter = state => state.filter;
 
 export const selectContacts = state => state.contacts;
 
-const selectContactsData = state => state.contacts.data;
+// const selectContactsData = state => state.contacts.data;
 
 // export const selectFilteredContacts = createSelector(
 //   [selectContactsData],
